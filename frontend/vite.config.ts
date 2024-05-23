d0e274e827a7d231af0f18560c1f2cd60024cb39
Sebastian Mohr <sebastian@mohrenclan.de>
2024-05-23 13:12:17 +0200

    Added some node specific files to gitignore and dockerignore.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import pluginPurgeCss from "vite-plugin-purgecss-updated-v5";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        TanStackRouterVite(),
-        pluginPurgeCss({
-            variables: true,
-        }),
-    ],
+    plugins: [react(), TanStackRouterVite()],
 });
