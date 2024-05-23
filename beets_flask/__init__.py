7780c20b86dda1643fe43c1eef71db504699e4e8
Sebastian Mohr <sebastian@mohrenclan.de>
2024-05-23 13:10:32 +0200

    Switched to factory flask app, this should allow for an easier switch to deployment later on.

--- beets_flask/__init__.py
+++ beets_flask/__init__.py
@@ -1,13 +1,38 @@
-from . import routes
-from . import beets_tags
-from . import beets_tasks
-from . import beets_sessions
-from . import disk
-from . import utility
-from ._version import __version__
+import os
 
-# for `export FLASK_APP=...` to work (without specifying submodules) we need the app variable here:
+from flask import Flask
+from flask_rq2 import RQ
+from flask_sqlalchemy import SQLAlchemy
+from .logging import setup_logging
 
-app = utility.app
-beets_tags.init()
-disk.init()
+
+def create_app():
+
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Setup logging
+    setup_logging(app)
+
+    # sqlite
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        "sqlite://///home/beetle/beets-flask-sqlite.db?timeout=5"
+    )
+    db = SQLAlchemy()
+    db.init_app(app)
+
+    # redis, workers
+    rq = RQ()
+    rq.init_app(app)
+    # we want to update the download table only when needed.
+    # redis connection also needed for sse
+    app.config["REDIS_URL"] = "redis://localhost"
+
+    # Register blueprints
+    from .backend import backend_bp
+    from .frontend import frontend_bp
+
+    app.register_blueprint(backend_bp)
+    app.register_blueprint(frontend_bp)
+
+    return app, db, rq
