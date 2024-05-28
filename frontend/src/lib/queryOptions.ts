6bc2e3badbdaf6dac7a697d12fd78f38bfe64f7e
Sebastian Mohr <sebastian@mohrenclan.de>
2024-05-28 22:18:03 +0200

    Added some notes on why multiple files for query and fetch

--- frontend/src/lib/queryOptions.ts
+++ frontend/src/lib/queryOptions.ts
@@ -11,7 +11,12 @@ export const inboxQueryOptions = () =>
 // PS 24-05-28: what do we gain from encapsulating this another time via Options?
 // Fair enough, we have the query key for hashing.
 // but why do we separate options and fetch into two files :P ?
-export const fsPathQueryOptions = (path:string) =>
+// SBM 24-05-28: For now it does not matter too much, but
+// think about a fetch with options and pagination, etc.
+// this blows up the fetch functions alot
+// additionally I like to keep all the options in one place
+// to have an overview of all the queries in the app
+export const fsPathQueryOptions = (path: string) =>
     queryOptions({
         queryKey: ["inbox", "path", path],
         queryFn: () => fetchFsPath(path),
