85585e688a159cc3db2fd26a8c70b4a0e44a67d7
Sebastian Mohr <sebastian@mohrenclan.de>
2024-05-29 20:39:11 +0200

    Added ts paths for @ syntax

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), TanStackRouterVite()],
+    plugins: [tsconfigPaths(), react(), TanStackRouterVite()],
 });
