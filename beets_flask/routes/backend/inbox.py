b50b7ca7d38a726277c590ef3f5d0f5aa2d1c92b
Sebastian Mohr <sebastian@mohrenclan.de>
2024-05-29 12:35:32 +0200

    Added a stats inbox api route, might need some cleanup

--- beets_flask/routes/backend/inbox.py
+++ beets_flask/routes/backend/inbox.py
@@ -14,7 +14,7 @@ def get_all():
     use_cache: bool = True
     """
 
-    use_cache = bool(request.args.get("use_cache", True))
+    use_cache = bool(request.args.get("use_cache", False))
     inbox = get_inbox_dict(use_cache=use_cache)
     log.debug(f"returning inbox {inbox=}")
 
@@ -27,3 +27,39 @@ def get_folder(folder):
     Get all subfolders from of the specified one
     """
     return path_to_dict("/" + folder, relative_to="/" + folder)
+
+
+import os
+from pathlib import Path
+
+inbox_dir = os.environ.get("INBOX", "/music/inbox")
+
+
+@inbox_bp.route("/stats", methods=["GET"])
+@inbox_bp.route("/stats/<path:folder>", methods=["GET"])
+def compute_stats(folder=None):
+    """
+    Compute the stats for the inbox folder
+
+    # Path parameters
+    folder: str (optional) - The folder to compute stats for
+
+    """
+    if not folder:
+        folder = inbox_dir
+    else:
+        folder = os.path.join(inbox_dir, folder)
+
+    folder = Path(folder)
+
+    count = 0
+    size_bytes = 0
+
+    # TODO: add all audio files
+    with os.scandir(folder) as it:
+        for entry in it:
+            if entry.is_file() and entry.name.endswith((".mp3", ".flac")):
+                count += 1
+                size_bytes += entry.stat().st_size
+
+    return jsonify({"nFiles": count, "size": size_bytes})
