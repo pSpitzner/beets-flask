e45a5c9a1cb85c2f25614f76a90b4c14c6430b90
Sebastian Mohr <sebastian@mohrenclan.de>
2025-01-26 12:06:16 +0100

    Fixed tests, i.e. added async and awaits to client

--- backend/tests/test_routes/test_tag.py
+++ backend/tests/test_routes/test_tag.py
@@ -23,62 +23,72 @@ def tags():
 
 class TestTagEndpoints:
 
-    def test_add_tag(self, client):
+    @pytest.mark.asyncio
+    async def test_add_tag(self, client):
         data = {"kind": "import", "folder": "/some/folder"}
-        response = client.post("/api_v1/tag/add", json=data)
-        data = response.get_json()
+        response = await client.post("/api_v1/tag/add", json=data)
+        data = await response.get_json()
         assert response.status_code == 200
         assert data["message"] == "1 tags added as kind: import"
 
-    def test_get_all_tags(self, client, tags):
-        response = client.get("/api_v1/tag/")
-        data = response.get_json()
+    @pytest.mark.asyncio
+    async def test_get_all_tags(self, client, tags):
+        response = await client.get("/api_v1/tag/")
+        data = await response.get_json()
         assert response.status_code == 200
         assert isinstance(data, list)
         assert len(data) == len(tags)
         for tag in tags:
             assert tag.get("id") in [tag.get("id") for tag in data]
 
-    def test_get_tag_by_id(self, client, tags):
+    @pytest.mark.asyncio
+    async def test_get_tag_by_id(self, client, tags):
         for tag in tags:
-            response = client.get(f"/api_v1/tag/id/{tag['id']}")
-            data = response.get_json()
+            response = await client.get(f"/api_v1/tag/id/{tag['id']}")
+            data = await response.get_json()
             assert response.status_code == 200
             assert isinstance(data, dict)
             assert_same_tag(tag, data)
 
-    def test_delete_tag_by_id(self, client, tags):
+    @pytest.mark.asyncio
+    async def test_delete_tag_by_id(self, client, tags):
         for tag in tags:
-            response = client.delete(f"/api_v1/tag/id/{tag['id']}")
-            data = response.get_json()
+            response = await client.delete(f"/api_v1/tag/id/{tag['id']}")
+            data = await response.get_json()
             assert response.status_code == 200
             assert data["message"] == "Tag deleted"
 
         # Test if tags are deleted
-        response = client.delete(f"/api_v1/tag/id/{tags[0]['id']}")
-        data = response.get_json()
+        response = await client.delete(f"/api_v1/tag/id/{tags[0]['id']}")
+        data = await response.get_json()
         assert response.status_code == 404
         assert data["message"] == "Tag not found"
 
-    def test_get_tag_by_folder_path(self, client, tags):
+    @pytest.mark.asyncio
+    async def test_get_tag_by_folder_path(self, client, tags):
         for tag in tags:
             print({tag["album_folder"]})
-            response = client.get(f"/api_v1/tag/path/{tag['album_folder'][1:]}")
-            data = response.get_json()
+            response = await client.get(f"/api_v1/tag/path/{tag['album_folder'][1:]}")
+            data = await response.get_json()
             assert response.status_code == 200
             assert isinstance(data, dict)
             assert_same_tag(tag, data)
 
-    def test_delete_tag_by_folder_path(self, client, tags):
+    @pytest.mark.asyncio
+    async def test_delete_tag_by_folder_path(self, client, tags):
         for tag in tags:
-            response = client.delete(f"/api_v1/tag/path/{tag['album_folder'][1:]}")
-            data = response.get_json()
+            response = await client.delete(
+                f"/api_v1/tag/path/{tag['album_folder'][1:]}"
+            )
+            data = await response.get_json()
             assert response.status_code == 200
             assert data["message"] == "Tag deleted"
 
         # Test if tags are deleted
-        response = client.delete(f"/api_v1/tag/path/{tags[0]['album_folder'][1:]}")
-        data = response.get_json()
+        response = await client.delete(
+            f"/api_v1/tag/path/{tags[0]['album_folder'][1:]}"
+        )
+        data = await response.get_json()
         assert response.status_code == 404
         assert data["message"] == "Tag not found"
 
