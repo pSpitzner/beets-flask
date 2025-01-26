273827a38cabc7dafb88dc25b9fe0dab9b9d8096
Sebastian Mohr <sebastian@mohrenclan.de>
2025-01-26 12:06:16 +0100

    Fixed tests, i.e. added async and awaits to client

--- backend/tests/test_routes/test_errors.py
+++ backend/tests/test_routes/test_errors.py
@@ -1,35 +1,43 @@
+import pytest
+
+
 class TestErrorHandling:
 
-    def test_invalid_usage(self, client):
-        response = client.get("/api_v1/error/invalidUsage")
-        data = response.get_json()
+    @pytest.mark.asyncio
+    async def test_invalid_usage(self, client):
+        response = await client.get("/api_v1/error/invalidUsage")
+        data = await response.get_json()
         assert response.status_code == 400
         assert data["error"] == "Bad request"
         assert data["message"] == "This is a bad request"
 
-    def test_not_implemented(self, client):
-        response = client.get("/api_v1/error/notImplemented")
-        data = response.get_json()
+    @pytest.mark.asyncio
+    async def test_not_implemented(self, client):
+        response = await client.get("/api_v1/error/notImplemented")
+        data = await response.get_json()
         assert response.status_code == 501
         assert data["error"] == "Not implemented"
 
-    def test_config_error(self, client):
-        response = client.get("/api_v1/error/configError")
-        data = response.get_json()
+    @pytest.mark.asyncio
+    async def test_config_error(self, client):
+        response = await client.get("/api_v1/error/configError")
+        data = await response.get_json()
         assert response.status_code == 400
         assert data["error"] == "Bad request"
         assert data["message"] == "Configuration Error"
 
-    def test_file_not_found(self, client):
-        response = client.get("/api_v1/error/fileNotFound")
-        data = response.get_json()
+    @pytest.mark.asyncio
+    async def test_file_not_found(self, client):
+        response = await client.get("/api_v1/error/fileNotFound")
+        data = await response.get_json()
         assert response.status_code == 404
         assert data["error"] == "File not found"
         assert data["message"] == "This is a file not found error"
 
-    def test_generic_error(self, client):
-        response = client.get("/api_v1/error/genericError")
-        data = response.get_json()
+    @pytest.mark.asyncio
+    async def test_generic_error(self, client):
+        response = await client.get("/api_v1/error/genericError")
+        data = await response.get_json()
         assert response.status_code == 500
         assert data["error"] == "Internal server error"
         assert "message" in data
