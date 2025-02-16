6270483c5809c71c7cf876f8180b32fc7d55cda6
Sebastian Mohr <sebastian@mohrenclan.de>
2025-02-16 15:43:20 +0100

    A bit of changes to our logger. I added a different log for the file logging and the stdout logger. Should allow to debug using the file and keep the stdout relatively clean.

--- backend/beets_flask/logger.py
+++ backend/beets_flask/logger.py
@@ -1,30 +1,58 @@
 import logging
+import logging.config
 import os
 
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "standard": {
+            # https://docs.python.org/3/library/logging.html#logrecord-attributes
+            "format": "[%(levelname)-5s] %(name)s: %(message)s"
+        },
+        "debug": {
+            "format": "%(relativeCreated)-8d [%(levelname)-5s] %(name)s %(filename)-8s:%(lineno)d: %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "standard",
+            "stream": "ext://sys.stdout",
+        },
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "DEBUG",
+            "formatter": "debug",
+            "filename": os.environ.get("LOG_FILE_WEB", "/repo/log/for_web.log"),
+            "maxBytes": 1048576,  #
+            "backupCount": 3,
+        },
+    },
+    "loggers": {
+        "": {  # root logger
+            "handlers": ["console", "file"],
+            "level": os.getenv("LOG_LEVEL_OTHERS", logging.WARNING),
+            "propagate": False,
+        },
+        "beets-flask": {
+            "handlers": ["console", "file"],
+            "level": os.getenv("LOG_LEVEL_BEETSFLASK", logging.DEBUG),
+            "propagate": False,
+        },
+    },
+}
 
-def setup_logging() -> None:
-    global log_file_for_web
-    global log
-
-    log_file_for_web = os.environ.get("LOG_FILE_WEB", "/repo/log/for_web.log")
-
-    # https://docs.python.org/3/library/logging.html#logrecord-attributes
-    logging.basicConfig(
-        format="%(relativeCreated)d [%(levelname)-4s] %(name)s - %(message)s %(filename)s:%(lineno)d",
-        level=os.getenv("LOG_LEVEL_OTHERS", logging.WARNING),
-    )
-    log = logging.getLogger("beets-flask")
-    log.setLevel(os.getenv("LOG_LEVEL_BEETSFLASK", logging.DEBUG))
-
-    # Overwrite loggers for redis workers
-    rq_name = os.getenv("RQ_JOB_ID", None)
-    if rq_name:
-        log = log.getChild(rq_name[0:4])
-        log.setLevel(os.getenv("LOG_LEVEL_BEETSFLASK_REDIS", logging.INFO))
-
-    log.info("Logging initialized")
+logging.config.dictConfig(LOGGING_CONFIG)
 
 
-log: logging.Logger
-log_file_for_web = None
-setup_logging()
+log = logging.getLogger("beets-flask")
+
+# Redis workers will have a different logger
+rq_name = os.getenv("RQ_JOB_ID", None)
+if rq_name:
+    log = log.getChild(rq_name[0:4])
+    log.setLevel(os.getenv("LOG_LEVEL_BEETSFLASK_REDIS", logging.DEBUG))
+
+log.debug("Logging configured!")
