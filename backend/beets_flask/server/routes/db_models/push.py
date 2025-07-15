6d9a3d76b667058e1789767eee121799edf5624b
Sebastian Mohr <sebastian@mohrenclan.de>
2025-07-15 17:13:38 +0200

    Added notification route blueprint that exposes the push notification db models.

---
+++ backend/beets_flask/server/routes/db_models/push.py
@@ -0,0 +1,182 @@
+from typing import Any
+
+from quart import request
+
+from beets_flask.database import db_session_factory
+from beets_flask.database.models import PushSettings, PushSubscription, PushWebHook
+from beets_flask.server.routes.exception import InvalidUsageException
+from beets_flask.server.utility import pop_query_param
+
+from .base import ModelAPIBlueprint
+
+
+class WebHookBlueprint(ModelAPIBlueprint[PushWebHook]):
+    def __init__(self):
+        super().__init__(PushWebHook, url_prefix="/webhook")
+        self.register_routes()
+
+    def register_routes(self):
+        """Register the routes for the blueprint."""
+        super()._register_routes()
+        self.blueprint.route("/", methods=["POST"])(self.upsert)
+
+    async def upsert(self):
+        """Upsert a push webhook."""
+        params = await request.get_json()
+        id, url, method, headers, params, body, settings = self._parse_webhook_params(
+            params
+        )
+
+        if not url:
+            raise InvalidUsageException(
+                "Missing 'url' parameter in webhook subscription",
+                status_code=400,
+            )
+        if not method:
+            raise InvalidUsageException(
+                "Missing 'method' parameter in webhook subscription",
+                status_code=400,
+            )
+
+        # Upsert webhook
+        with db_session_factory() as db_session:
+            if id:
+                # update
+                if web_hook := PushWebHook.get_by(
+                    PushWebHook.id == id, session=db_session
+                ):
+                    web_hook.url = url
+                    web_hook.method = method
+                    web_hook.headers = headers
+                    web_hook.params = params
+                    web_hook.body = body
+                    if settings:
+                        web_hook.settings.update_from_dict(settings)
+                    db_session.commit()
+                    return web_hook.to_dict(), 200
+                else:
+                    raise InvalidUsageException(
+                        f"Webhook with id '{id}' does not exist",
+                        status_code=404,
+                    )
+            # insert
+            web_hook = PushWebHook(
+                url=url,
+                method=method,
+                headers=headers,
+                params=params,
+                body=body,
+                settings=PushSettings.from_dict(settings or {}),
+            )
+            db_session.add(web_hook)
+            db_session.commit()
+
+            return web_hook.to_dict(), 201
+
+    def _parse_webhook_params(self, params: Any):
+        """Parse the parameters for the webhook subscription."""
+        if not isinstance(params, dict):
+            raise InvalidUsageException(
+                "Invalid parameters provided for webhook subscription",
+                status_code=400,
+            )
+
+        # Standard PushSubscription fields
+        id = pop_query_param(params, "id", str, default=None)
+        url = pop_query_param(params, "url", str, default=None)
+        method = pop_query_param(params, "method", str, default=None)
+
+        # Optional fields for webhook
+        headers = pop_query_param(params, "headers", dict, default=None)
+        params = pop_query_param(params, "params", dict, default=None)
+        body = pop_query_param(params, "body", dict, default=None)
+
+        # Settings for the webhook
+        settings = pop_query_param(params, "settings", dict, default=None)
+
+        return (
+            id,
+            url,
+            method,
+            headers,
+            params,
+            body,
+            settings,
+        )
+
+
+class SubscriptionBlueprint(ModelAPIBlueprint[PushSubscription]):
+    def __init__(self):
+        super().__init__(PushSubscription, url_prefix="/subscription")
+        self.register_routes()
+
+    def register_routes(self):
+        """Register the routes for the blueprint."""
+        super()._register_routes()
+        self.blueprint.route("/", methods=["POST"])(self.upsert)
+
+    async def upsert(self):
+        """Upsert a push subscription."""
+        params = await request.get_json()
+        endpoint, expiration_time, keys, settings = self._parse_subscription_params(
+            params
+        )
+
+        if not endpoint or not keys:
+            raise InvalidUsageException(
+                "Missing 'endpoint' or 'keys' parameter in subscription",
+                status_code=400,
+            )
+
+        # Upsert subscription (id == endpoint)
+        with db_session_factory() as db_session:
+            if subscription := PushSubscription.get_by(
+                PushSubscription.id == endpoint, session=db_session
+            ):
+                # update
+                subscription.keys = keys
+                subscription.expiration_time = expiration_time
+                if settings:
+                    subscription.settings.update_from_dict(settings)
+                db_session.commit()
+                return subscription.to_dict(), 200
+
+            # insert
+            subscription = PushSubscription(
+                id=endpoint,
+                keys=keys,
+                expiration_time=expiration_time,
+                settings=PushSettings.from_dict(settings or {}),
+            )
+            db_session.add(subscription)
+            db_session.commit()
+            return subscription.to_dict(), 201
+
+    def _parse_subscription_params(self, params: Any):
+        """Parse the parameters for the push API."""
+        if not isinstance(params, dict):
+            raise InvalidUsageException(
+                "Invalid parameters provided for subscription",
+                status_code=400,
+            )
+
+        # Standard PushSubscription fields
+        endpoint = pop_query_param(params, "endpoint", str, default=None)
+        expiration_time = pop_query_param(params, "expirationTime", int, default=None)
+        keys = pop_query_param(params, "keys", dict, default=None)
+
+        # Extra options for the subscription (i.e. which notifications to receive)
+        settings = pop_query_param(params, "settings", dict, default=None)
+
+        if len(params) > 0:
+            raise InvalidUsageException(
+                "Invalid parameters provided for subscription",
+                status_code=400,
+            )
+
+        return (
+            endpoint,
+            expiration_time,
+            keys,
+            settings,
+        )
---
+++ backend/beets_flask/server/routes/notifications.py
@@ -0,0 +1,199 @@
+"""Notifications api.
+
+Notification allow to receive updates about changes in the backend.
+For instance new tags generated or new imported albums.
+
+Currently we support web push notifications and arbitrary webhooks.
+"""
+
+from __future__ import annotations
+
+import base64
+import json
+import select
+from typing import Literal, NamedTuple, TypedDict
+
+import ecdsa
+from pywebpush import WebPushException, webpush
+from quart import Blueprint, Quart, request
+
+from beets_flask.config.beets_config import get_bf_config_dir
+from beets_flask.database import db_session_factory
+from beets_flask.database.models import PushSubscription
+from beets_flask.logger import log
+from beets_flask.server.routes.exception import InvalidUsageException
+from beets_flask.server.routes.inbox import get_inbox_for_path
+
+from .db_models.push import SubscriptionBlueprint, WebHookBlueprint
+
+notification_bp = Blueprint("notifications", __name__, url_prefix="/notifications")
+
+# -------------------------------- Vapid keys -------------------------------- #
+# Vapid keys are used for web push notifications to authenticate the sender.
+# TODO: Add a way to add a sub information to the VAPID keys, e.g. an email address.
+# Maybe config?
+
+
+@notification_bp.route("/vapid_key", methods=["GET"])
+def get_vapid_key():
+    """Get the public VAPID key for web push notifications."""
+    keypair = get_vapid_keypair()
+
+    return (
+        {"key": keypair.public},
+        200,
+        {"Content-Type": "application/json"},
+    )
+
+
+class VapidKeyPair(NamedTuple):
+    private: str
+    public: str
+
+
+def get_vapid_keypair():
+    """Get the VAPID keys for web push notifications."""
+    file = get_bf_config_dir() / "vapid_keys.json"
+    if not file.exists():
+        # If the file does not exist, generate a new keypair
+        keys = _generate_vapid_keypair()
+        with open(file, "w") as f:
+            f.write(
+                json.dumps(
+                    {
+                        "private_key": keys.private,
+                        "public_key": keys.public,
+                    }
+                )
+            )
+        return keys
+    with open(file, "r") as f:
+        data = json.load(f)
+        return VapidKeyPair(
+            private=data["private_key"],
+            public=data["public_key"],
+        )
+
+
+def _generate_vapid_keypair():
+    # See https://datatracker.ietf.org/doc/rfc8292/
+    pk = ecdsa.SigningKey.generate(curve=ecdsa.NIST256p)
+    vk = pk.get_verifying_key()
+
+    return VapidKeyPair(
+        private=base64.urlsafe_b64encode(pk.to_string()).strip(b"=").decode("utf-8"),
+        public=base64.urlsafe_b64encode(b"\x04" + vk.to_string())  # type: ignore
+        .strip(b"=")
+        .decode("utf-8"),
+    )
+
+
+# ----------------------------------- Tests ---------------------------------- #
+
+
+@notification_bp.route("/notify_test", methods=["POST"])
+async def notify_test():
+    """Send a test notification to all subscribed clients."""
+    params = await request.get_json() or {}
+
+    inbox = get_inbox_for_path(
+        "/music/inbox/to/album",
+    )
+    # Create a test notification
+    notification = TaggedNotification(
+        hash="test_hash",
+        path="/music/inbox/to/album",
+        type="tagged",
+        nCandidates=10,
+        bestCandidate="Test Album - Test Artist",
+        bestCandidateMatch=0.95,
+        inboxPath=inbox["path"] if inbox else None,
+    )
+
+    if len(params) > 0:
+        raise InvalidUsageException(
+            "Invalid parameters provided for notification",
+            status_code=400,
+        )
+
+    # Push the notification
+    push_notification(notification)
+
+    return "Test notification sent successfully", 200
+
+
+from sqlalchemy import select
+
+
+def push_notification(
+    notification: Notification,
+):
+    """Push a notification to all subscribed clients."""
+    vapid_keys = get_vapid_keypair()
+
+    # Get all subscriptions from the database
+    with db_session_factory() as db_session:
+        stmt = select(PushSubscription)
+        subscriptions = db_session.scalars(stmt).all()
+
+        if not subscriptions:
+            return
+
+        # Try to send the notification to each subscription
+        # it the status is 410 Gone, remove the subscription
+
+        for subscription in subscriptions:
+            keys = subscription.keys
+            try:
+                webpush(
+                    subscription_info={
+                        "endpoint": subscription.id,
+                        "keys": {k: v for k, v in keys.items()},
+                    },
+                    data=json.dumps(notification),
+                    vapid_private_key=vapid_keys.private,
+                    vapid_claims={"sub": "mailto:test@test.de"},
+                )
+            except WebPushException as e:
+                if e.response is not None and e.response.status_code == 410:
+                    # Subscription is no longer valid, remove it
+                    instance = PushSubscription.get_by(
+                        PushSubscription.id == subscription.id,
+                        session=db_session,
+                    )
+                    if instance:
+                        db_session.delete(instance)
+                        db_session.commit()
+                else:
+                    log.exception(
+                        f"Failed to send notification to {subscription.id}", e
+                    )
+    log.debug(f"Notification sent to {len(subscriptions)} subscribers")
+
+
+# ---------------------------- Notification types ---------------------------- #
+
+
+class Notification(TypedDict):
+    """Notification for tagged albums."""
+
+    hash: str
+    path: str
+
+
+class TaggedNotification(Notification):
+    """Notification for tagged albums with a type."""
+
+    type: Literal["tagged"]
+    nCandidates: int
+    bestCandidate: str  # title - artist
+    bestCandidateMatch: float  # match percentage (1.0 = 100%)
+    inboxPath: str | None  # path to the inbox this folder is in
+
+
+def register_notifications(app: Blueprint | Quart):
+    """Register the push models with the app."""
+
+    notification_bp.register_blueprint(WebHookBlueprint().blueprint)
+    notification_bp.register_blueprint(SubscriptionBlueprint().blueprint)
+    app.register_blueprint(notification_bp)
