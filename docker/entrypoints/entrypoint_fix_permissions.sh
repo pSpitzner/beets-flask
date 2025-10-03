08651339db51bb9375cf0eb2e89198a49b8b1cec
pSpitzner <paul.spitzner@ds.mpg.de>
2025-10-03 14:53:45 +0200

    a few changes: - dialog styling - enabled dialog dropzone - excluding .git from repo permission fix
    a few bugs around uploading:
    - we can add the same file multiple times
    - we can add files after the upload finished :P

--- docker/entrypoints/entrypoint_fix_permissions.sh
+++ docker/entrypoints/entrypoint_fix_permissions.sh
@@ -4,6 +4,7 @@ if [ ! -z "$USER_ID" ] && [ ! -z "$GROUP_ID" ]; then
     groupmod -g $GROUP_ID beetle
     usermod -u $USER_ID -g $GROUP_ID beetle > /dev/null 2>&1
     chown -R beetle:beetle /home/beetle
-    chown -R beetle:beetle /repo
     chown -R beetle:beetle /logs
+    # chown /repo but exclude .git, useful for dev container.
+    find /repo -not -path "*/.git/*" -not -name ".git" -exec chown beetle:beetle {} +
 fi
