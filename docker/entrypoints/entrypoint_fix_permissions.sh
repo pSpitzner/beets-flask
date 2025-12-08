4af6db6d7aaec68ef12f5acef3b6f5523efd32d0
djdubd <dandrallid@gmail.com>
2025-12-08 16:31:32 -0600

    Added ability to define more groups to take care of ACLs. New EXTRA_GROUPS env var (#239)
    - Add EXTRA_GROUPS support for beetle user permissions
    - Update docker/entrypoints/entrypoint_fix_permissions.sh
    - Add group name validation
    - Warn user if named group exists with different gid
    - Add explicit check for user having existing group membership.
    - Added changelog entry
    - Separated group_add into its own entrypoint script (called by fix_permissions one)
    - Split group_add logic into two functions
    - Removed old Testing parts from Dockerfile and entrypoint
    - Improved chown performance on macos
    
    closes #234
    ---------
    
    Co-authored-by: djdubd <2773193+djdubd@users.noreply.github.com>
    Co-authored-by: pSpitzner <github@makeitso.one>

--- docker/entrypoints/entrypoint_fix_permissions.sh
+++ docker/entrypoints/entrypoint_fix_permissions.sh
@@ -1,9 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+
+# this script runs both, in dev and in prod, so we have to check where we
+# can source common.sh from.
+if [ -f ./common.sh ]; then
+    source ./common.sh
+elif [ -f ./docker/entrypoints/common.sh ]; then
+    source ./docker/entrypoints/common.sh
+fi
 
 if [ ! -z "$USER_ID" ] && [ ! -z "$GROUP_ID" ]; then
+    log "Fixing permissions as '$(whoami)' with UID $(id -u) and GID $(id -g)"
+    log "Setting beetle user to $USER_ID:$GROUP_ID"
     groupmod -g $GROUP_ID beetle
     usermod -u $USER_ID -g $GROUP_ID beetle > /dev/null 2>&1
-    chown -R beetle:beetle /home/beetle
-    chown -R beetle:beetle /repo
-    chown -R beetle:beetle /logs
+    log "User beetle now has $(id beetle)"
+    find /home/beetle ! -user beetle -exec chown beetle:beetle {} +
+    find /logs ! -user beetle -exec chown beetle:beetle {} +
+    find /repo ! -user beetle -exec chown beetle:beetle {} +
+    log "Done fixing permissions"
+else
+    log "No USER_ID and GROUP_ID set, skipping permission updates"
+fi
+
+# add groups
+if [ -f ./entrypoint_add_groups.sh ]; then
+    source ./entrypoint_add_groups.sh
+elif [ -f ./docker/entrypoints/entrypoint_add_groups.sh ]; then
+    source ./docker/entrypoints/entrypoint_add_groups.sh
 fi
