8c3fac28f88d8717db68c33579ac2aa2893aabd5
Benoît Dardenne <benoit.dardenne@gmail.com>
2026-01-06 17:58:58 +0100

    Fix tsc inferring restrictive type for navItems.

--- frontend/src/components/frontpage/navbar.tsx
+++ frontend/src/components/frontpage/navbar.tsx
@@ -153,19 +153,19 @@ function NavTabs() {
         basePath += '/' + location.pathname.split('/')[2];
     }
 
-    const navItems = [
-        { label: 'Home', icon: <Home />, to: '/' as const },
-        { label: 'Inbox', icon: <Inbox />, to: '/inbox' as const },
-        //{ label: "Session", icon: <Inbox />, to: "/sessiondraft" as const },
-        {label: 'Library', icon: <Library/>, to: '/library/browse' as const},
-        {label: 'Search', icon: <Search/>, to: '/library/search' as const},
+    const navItems: StyledTabProps[] = [
+        { label: 'Home', icon: <Home />, to: '/' },
+        { label: 'Inbox', icon: <Inbox />, to: '/inbox'},
+        //{ label: "Session", icon: <Inbox />, to: '/sessiondraft'},
+        {label: 'Library', icon: <Library/>, to: '/library/browse'},
+        {label: 'Search', icon: <Search/>, to: '/library/search'},
     ];
 
     if (config.gui.terminal.enable) {
         navItems.push({
             label: '',
             icon: <Terminal stroke={theme.palette.primary.main} />,
-            to: '/terminal' as const,
+            to: '/terminal',
         });
     }
 
